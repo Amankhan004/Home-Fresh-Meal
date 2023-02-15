76073244591f4904ad123a6b6bfd6c0c05904d1c
Amankhan004 <ak1389158@gmail.com>
2023-02-14 23:30:19 -0800

    leaving-workspace

--- assets/js/style.js
+++ assets/js/style.js
@@ -24,4 +24,26 @@ rotateimg.forEach(element => {
    })    
 });
 
-AOS.init();
+
+window.addEventListener("scroll",  ()=> {
+   var scroll = document.querySelector(".scrollTop");
+   scroll.classList.toggle("active", window.scrollY > 2300);
+
+ });
+
+ $('.owl-carousel').owlCarousel({
+   loop:true,
+   margin:10,
+   nav:true,
+   responsive:{
+       0:{
+           items:1
+       },
+       600:{
+           items:3
+       },
+       1000:{
+           items:5
+       }
+   }
+})
